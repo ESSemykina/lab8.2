44160ddcad1a1992aa945772502b9a72bc535f67
ESSemykina <90087300+ESSemykina@users.noreply.github.com>
2022-05-11 10:52:23 +0200

    Add files via upload

---
+++ lab8.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <cassert>
+#include "Matrix.hpp"
+
+
+using ks::Mat22d;
+using ks::Vec2d;
+
+int main()
+{
+    std::cout << "=== Test 1 ===" << std::endl;
+    {
+        Mat22d A({ {
+                 {1,2},
+                 {3,4}
+        } });
+
+        Vec2d X({ {
+                {1},
+                {1}
+         } });
+
+        auto B = A * X;
+
+        assert(B.get(0, 0) == 3);
+        assert(B.get(1, 0) == 7);
+    }
+    std::cout << "Done!" << std::endl;
+
+    
+
+    
+    std::cout << "=== Test 2 ===" << std::endl;
+    {
+        Mat22d A({ {
+                 {1,2},
+                 {3,4}
+         } });
+
+        auto B = A.transposition();
+        assert(B.get(0, 0) == 1);
+        assert(B.get(0, 1) == 3);
+        assert(B.get(1, 0) == 2);
+        assert(B.get(1, 1) == 4);
+
+    }
+    std::cout << "Done!" << std::endl;
+
+    std::cout << "=== Test 3  ===" << std::endl;
+    {
+        Mat22d A({ {
+                 {1,2},
+                 {3,4}
+         } });
+
+        auto B = A.det();
+        assert(B == -2);
+    }
+    std::cout << "Done!" << std::endl;
+
+    std::cout << "=== Test 4 ===" << std::endl;
+    {
+
+            try
+            {
+                Mat22d A({ {
+                    {1,2},
+                    {3,4}
+            } });
+
+                auto B = A.inverse();
+            }
+            
+            catch (const std::exception& e)
+            {
+                std::cerr << e.what() << std::endl;
+            }
+
+    }
+    std::cout << "Done!" << std::endl;
+
+
+
+    return 0;
+}
